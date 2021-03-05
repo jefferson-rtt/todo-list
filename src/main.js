2450bffafb42be961b36e1555d05780726bbde4c
Jefferson-rtt <jefferson.rtt@outlook.com>
2021-03-05 12:23:22 -0300

    Add Function

--- src/main.js
+++ src/main.js
@@ -9,6 +9,10 @@ const app = createApp({
     data() {
         return {
             todos: [],
+            form: {
+                text: '',
+                done: false
+            }
         }
     },
     created() {
@@ -18,7 +22,31 @@ const app = createApp({
     methods: {
         async fetchTodos() {
             this.todos = await apiTodos.index()
+        },
+        async createTodo() {
+            const data = await apiTodos.store(this.form)
+            this.todos.push(data)
+
+            this.form.text = ""
+            this.form.done = false
+        },
+        async toggleTodoStatus(todo) {
+            const data = await apiTodos.update({
+                ...todo,
+                done: !todo.done,
+            })
+
+            const index = this.todos.findIndex(({ id }) => id === data.id)
+            this.todos[index] = data
+        },
+        async removeTodo(id) {
+            await apiTodos.remove({ id })
+
+            const index = this.todos.findIndex((todo) => todo.id === id)
+            this.todos.splice(index, 1)
+
         }
+
     },
 })
 
